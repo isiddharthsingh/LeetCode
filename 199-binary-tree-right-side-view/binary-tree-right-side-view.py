ace10c50d8f2f7ad5ae088b8f0783fb4cfcbad5b
Siddharth Singh <49589002+isiddharthsingh@users.noreply.github.com>
2024-03-31 01:01:19 -0400

    Time: 44 ms (9.18%) | Memory: 16.5 MB (45.35%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,16 +6,20 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node, depth, result):
-            if not node:
-                return
-            # If this is the first time we're at this depth, add the node value.
-            if depth == len(result):
-                result.append(node.val)
-            # Prefer right nodes first, then left nodes.
-            dfs(node.right, depth + 1, result)
-            dfs(node.left, depth + 1, result)
+        res = []
+        q = collections.deque([root])
 
-        result = []
-        dfs(root, 0, result)
-        return result
+        while q:
+            rightSide = None
+            qLen = len(q)
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if rightSide:
+                res.append(rightSide.val)
+        return res
