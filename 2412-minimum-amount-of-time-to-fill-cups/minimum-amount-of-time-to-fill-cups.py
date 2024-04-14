1077bfc5ffb18be26a4e6c8bd5bebe093409caea
Siddharth Singh <49589002+isiddharthsingh@users.noreply.github.com>
2024-04-13 21:29:57 -0400

    Time: 41 ms (31.95%) | Memory: 16.6 MB (48.05%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.py
@@ -1,14 +1,24 @@
+import heapq
+from typing import List
+
 class Solution:
     def fillCups(self, amount: List[int]) -> int:
-        count = 0 
-        amount.sort(reverse=True)
-
-        while amount[0] > 0:
-            if amount[1] > 0:
-                amount[0] -= 1
-                amount[1] -= 1
-            else:
-                amount[0] -=1
-            count+=1
-            amount.sort(reverse=True)
-        return (count)
+        if sum(amount) == 0:
+            return 0 
+        amount = [-a for a in amount]
+        heapq.heapify(amount)
+        total_time = 0
+        while len(amount) > 1:
+            first = -heapq.heappop(amount)
+            second = -heapq.heappop(amount)
+            first -= 1
+            second -= 1
+            total_time += 1
+            if first > 0:
+                heapq.heappush(amount, -first)
+            if second > 0:
+                heapq.heappush(amount, -second)
+        print(amount)
+        if amount:
+            total_time += -amount[0]
+        return total_time
