3113b2c044cce5571f1f857cf356f8ce9f1da36c
Siddharth Singh <49589002+isiddharthsingh@users.noreply.github.com>
2024-07-11 20:47:16 -0400

    Time: 40 ms (45.76%) | Memory: 16.6 MB (27.38%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,17 +6,6 @@
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        """if l1 is None:
-            return l2
-        if l2 is None:
-            return l1
-    
-        if l1.val <= l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2"""
         dummy = ListNode()
         curr = dummy
         # while l1 and l2 are not None
@@ -46,4 +35,14 @@ class Solution:
         # return dummy.next
         return dummy.next
 
-        
+        """if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+    
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2"""
