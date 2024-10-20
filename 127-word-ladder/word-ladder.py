cde154a0ea596e2fcae117656972ac55700274dc
Siddharth Singh <49589002+isiddharthsingh@users.noreply.github.com>
2024-10-20 00:25:47 -0400

    Time: 315 ms (53.17%) | Memory: 17.8 MB (87.55%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,33 +1,39 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList:
+        # Convert the word list to a set for O(1) lookups
+        word_set = set(wordList)
+
+        # If the endWord is not in the word set, return 0
+        if endWord not in word_set:
             return 0
         
-        nei = defaultdict(list)
-        wordList.append(beginWord)
+        # List of all possible characters for transformation
+        char_list = 'abcdefghijklmnopqrstuvwxyz'
+        # Initialize the queue with the beginWord and step count 1
+        queue = deque([(beginWord, 1)])
 
-        for word in wordList:
-            for j in range(len(word)):
-                pattern = word[:j]+"*"+word[j+1:]
-                nei[pattern].append(word)
+        while queue:
+            # Get the current word and the number of steps from the queue
+            curr_word, step = queue.popleft()
 
-        visit = set([beginWord])
-        q = deque([beginWord])
-        res = 1
+            # If the current word is the endWord, return the number of steps
+            if curr_word == endWord:
+                return step 
+            
+            # Try changing each character in the current word
+            for i in range(len(curr_word)):
+                for char in char_list:
+                    # Skip if the character is the same as the current one
+                    if curr_word[i] == char:
+                        continue
 
-        while q:
-            for i in range(len(q)):
-                word = q.popleft()
-                if word == endWord:
-                    return res
-                
-                for j in range(len(word)):
-                    pattern = word[:j]+"*"+word[j+1:]
-                    for neiword in nei[pattern]:
-                        if neiword not in visit:
-                            visit.add(neiword)
-                            q.append(neiword)
+                    # Create a new word by changing the current character
+                    next_word = curr_word[:i] + char + curr_word[i+1:]
 
-            res += 1
+                    # If the new word is in the word set, add it to the queue and remove it from the set
+                    if next_word in word_set:
+                        queue.append((next_word, step + 1))
+                        word_set.remove(next_word)
+        
+        # If no transformation sequence leads to the endWord, return 0
         return 0
-
